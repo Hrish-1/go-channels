cf024f9e5a40086de99afd56f977222a798b75f5
Hrish-1 <hrishikeshm997@gmail.com>
2022-09-18 01:33:16 +0530

    feat: added loading spinner

--- main.go
+++ main.go
@@ -1,97 +1,68 @@
 package main
 
 import (
+	"channels/pets"
 	"fmt"
-	"sync"
-	"time"
+	"os"
+
+	"github.com/charmbracelet/bubbles/spinner"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
-var userMap map[int]User = map[int]User{
-	1: {1, "John", "john@g.com", "9988773", 1},
-	2: {2, "Jim", "jim@g.com", "4433221", 2},
-	3: {3, "Mary", "mary@g.com", "4455667", 3},
-}
-
-var petMap map[int]Pet = map[int]Pet{
-	1: {1, "Max", "dog", User{}},
-	2: {2, "Fiona", "cat", User{}},
-	3: {3, "Bonnie", "cat", User{}},
-}
-
-func lookUpUsers(id int) User {
-	time.Sleep(500 * time.Millisecond)
-	return userMap[id]
-}
-
-func lookupPets(id int) Pet {
-	time.Sleep(500 * time.Millisecond)
-	return petMap[id]
-}
-
-type User struct {
-	id    int
-	name  string
-	email string
-	phone string
-	petId int
-}
-
-type Pet struct {
-	id      int
-	name    string
-	petType string
-	user    User
-}
-
-func getUsers(ids []int) chan User {
-	var wg sync.WaitGroup
-	c := make(chan User, len(ids))
-	wg.Add(len(ids))
-	for _, id := range ids {
-		go func(id int) {
-			c <- lookUpUsers(id)
-			wg.Done()
-		}(id)
-	}
-	wg.Wait()
-	close(c)
-	return c
-}
-
-func getPets(users []User) chan Pet {
-	var wg sync.WaitGroup
-	c := make(chan Pet, len(users))
-	wg.Add(len(users))
-	for _, user := range users {
-		go func(u User) {
-			pet := lookupPets(u.petId)
-			pet.user = u
-			c <- pet
-			wg.Done()
-		}(user)
-	}
-	wg.Wait()
-	close(c)
-	return c
-}
-
 func main() {
-	start := time.Now().UnixMilli()
-	ids := []int{1, 2, 3}
-	userChan := getUsers(ids)
-	var users []User
-	for user := range userChan {
-		users = append(users, user)
+	s := spinner.NewModel()
+	s.Spinner = spinner.Dot
+	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	initialModel := model{
+		spinner: s,
+		out:     petInfo(""),
+		loading: true,
 	}
-	petChan := getPets(users)
-	m := make(map[int]string)
-	for pet := range petChan {
-		str := fmt.Sprintf("| %d | %s | %s | %s | %s |", pet.user.id, pet.user.name, pet.user.email, pet.name, pet.petType)
-		m[pet.user.id] = str
+	err := tea.NewProgram(initialModel).Start()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
-	for _, id := range ids {
-		fmt.Println(m[id])
+}
+
+type petInfo string
+
+type model struct {
+	out     petInfo
+	spinner spinner.Model
+	loading bool
+}
+
+// Init implements tea.Model
+func (m model) Init() tea.Cmd {
+	return tea.Batch(spinner.Tick, fetchPetInfo)
+}
+
+// Update implements tea.Model
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case petInfo:
+		m.loading = false
+		m.out = msg
+		return m, tea.Quit
 	}
-	end := time.Now().UnixMilli()
-	fmt.Println(end - start)
+	if m.loading {
+		var cmd tea.Cmd
+		m.spinner, cmd = m.spinner.Update(msg)
+		return m, cmd
+	}
+	return m, nil
+}
+
+// View implements tea.Model
+func (m model) View() string {
+	if m.loading {
+		return fmt.Sprintf("%s fetching pet info... please wait", m.spinner.View())
+	}
+	return string(m.out)
+}
+
+func fetchPetInfo() tea.Msg {
+	return petInfo(pets.PetInfo())
 }
